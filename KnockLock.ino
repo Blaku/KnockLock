9a5619ec99588829f909060826274bf5d8fafc42
Thomas Höglund <thomas.hoeglund@gmail.com>
2016-08-30 11:16:09 +0300

    Now works with an electret instead of a switch.

--- KnockLock.ino
+++ KnockLock.ino
@@ -1,10 +1,17 @@
 /*
+  KnockLock by Thomas Höglund
+  Created 7.7.2016
+  Modified 30.8.2016
   License information: This code is released by Thomas Höglund (thomas.hoeglund@gmail.com) 
   under Creative Commons Attribution 4.0 International
 
   Summary:
-  This Arduino code recognizes melodies using a digital input. The index of the recognized melody is printed out.
+  This Arduino code recognizes melodies using an electret with amplifier as input. The index of the recognized melody is printed out.
+  The code was tested by knocking on a Sparkfun BOB-12758 electret with amplifier. 
   
+  Functionality:
+  - Record knocks until enough intevals (MELODY_LENGTH) were recorded or timeout reached
+  - Analyze relative lengths of intervals
   - Print out index of recognized melody
   - Blink quick error blinks if not recognized or long blink if recognized
   - Uncomment //printMelodyFloat(normalizedMelody); to read what you input so you can copy to new melodies
@@ -13,12 +20,17 @@
 #define	MELODY_LENGTH 8
 #define MELODIES 3
 #define TIMEOUT 10000
-#define ALLOWED_ERROR 1200
+#define ALLOWED_ERROR 1400
 #define DEBOUNCE_TIME 120
-#define LED_PIN 13
-#define INPUT_PIN 2
+#define LED_PIN 9 //could be 13 for on board LED some Arduinos
+#define INPUT_PIN A0
 #define PULSE_OUT_PIN 3
 #define MINIMUM_PULSE_LENGTH 100 //index 0=100ms pulse, 1=200ms and so on
+
+#define THRESHOLD 100
+#define SAMPLE_WINDOW 50
+#define AVERAGING_WINDOWS_LONG 70
+#define AVERAGING_WINDOWS_SHORT 7
 
 float melodies[MELODIES][MELODY_LENGTH] = {
   {1974.49,1757.65,857.14,951.53,1859.69,882.65,895.41,821.43},        //0: Bää bää vita lamm
@@ -30,13 +42,12 @@ void setup() {
   pinMode(LED_PIN, OUTPUT);
   pinMode(PULSE_OUT_PIN, OUTPUT);
   pinMode(INPUT_PIN, INPUT);
-  Serial.begin(19200);
+  Serial.begin(115200);
   /*
     Serial.println("Please store melody 0.");
     storeMelody(0);
     delay(2000);//Don't start recording the next melody immediately
   */
-
   //Serial.println("Initialized.");
 }
 
@@ -47,7 +58,7 @@ void loop() {
     //printMelodyUnsignedLong(rawMelody);
     float normalizedMelody[MELODY_LENGTH];
     normalizeMelody(rawMelody, normalizedMelody);
-    //printMelodyFloat(normalizedMelody);
+    printMelodyFloat(normalizedMelody);
     //compare to stored melodies
     float errors[MELODIES];
     for (byte i = 0; i < MELODIES; i++) {
@@ -60,7 +71,7 @@ void loop() {
     byte indexOfSmallestError = 0;
     float smallestError = 10000;
     for (byte i = 0; i < MELODIES; i++) {
-      //Serial.println("Total error for melody " + String(i) + ": " + String(errors[i]));
+      Serial.println("Total error for melody " + String(i) + ": " + String(errors[i]));
       if (errors[i] < smallestError) {
         smallestError = errors[i];
         indexOfSmallestError = i;
@@ -75,13 +86,13 @@ void loop() {
       //pulseOut(i);
     }
     else { //indicate failure
-      //Serial.println("Failed to recognize melody.");
+      Serial.println("Failed to recognize melody.");
       blinkFast();
     }
   }
   else {
     blinkFast();
-    //Serial.println("Timed out");
+    Serial.println("Timed out");
 
   }
 }
@@ -160,29 +171,73 @@ void blinkMelody(byte melodyIndex) {
 }
 
 bool recordMelody(unsigned long rawMelody[]) {
-  unsigned long startTime;
-  unsigned long lastTime;
+  int aud=0;
+  static int previousAudsLongTime[AVERAGING_WINDOWS_LONG];
+  static int previousAudsShortTime[AVERAGING_WINDOWS_LONG];
+  static int loopCountLong = 0;
+  static int loopCountShort = 0;
+  static int averageLong=300;
+  static int averageShort=300;
+  int maxAbsVol = 0;   // maximum absolute volume
+  unsigned long startTime=millis();
+  unsigned long lastKnock = millis();
+  boolean startTimeSet = false;
   boolean success = false;
+  byte i=0;
+  while(millis()-startTime < TIMEOUT){
+     loopCountLong+=1;
+     loopCountShort+=1;
+     loopCountLong%=AVERAGING_WINDOWS_LONG;
+     loopCountShort%=AVERAGING_WINDOWS_SHORT;
+     unsigned long startOfSampleWindow = millis();
+     maxAbsVol=0;
+     while(millis()-startOfSampleWindow < SAMPLE_WINDOW){
+         aud = analogRead(0);
+         if (abs(aud-averageLong) > maxAbsVol){
+           maxAbsVol = abs(aud-averageLong);
+         }
+         //delay(2); //allow ADC to settle? Too slow. 
+     }
+     previousAudsLongTime[loopCountLong]=maxAbsVol;
+     previousAudsShortTime[loopCountShort]=maxAbsVol;
+     unsigned int sum=0;
+     for(int i=0;i<AVERAGING_WINDOWS_LONG;i++){
+      sum+=previousAudsLongTime[i];
+     }
+     averageLong = sum/AVERAGING_WINDOWS_LONG;
+     sum=0;
+     for(int i=0;i<AVERAGING_WINDOWS_SHORT;i++){
+      sum+=previousAudsShortTime[i];
+     }
+     averageShort = sum/AVERAGING_WINDOWS_SHORT;
 
-  while (digitalRead(INPUT_PIN) == LOW) {} //Wait for digital input pulse (e.g. knock or door handle input)
-  startTime = millis();
-  lastTime = startTime;
-  //Serial.println("Started recording.");
-  for (byte i = 0; i < MELODY_LENGTH; i++) { //record melody
-    while (digitalRead(INPUT_PIN) == HIGH && millis() - startTime < TIMEOUT) {} //wait for pulse to go LOW
-    if (millis() - startTime >= TIMEOUT)
-      return false;
-    delay(DEBOUNCE_TIME);//debounce falling edge
-    while (digitalRead(INPUT_PIN) == LOW && millis() - startTime < TIMEOUT) {} //wait for next pulse to go HIGH
-    if (millis() - startTime >= TIMEOUT)
-      return false;
-    rawMelody[i] = millis() - lastTime;
-    //Serial.println(String(i) + ": " + String(rawMelody[i]));
-    if (i == MELODY_LENGTH) //if whole melody successfully recorded
-      return true;
-    lastTime = millis();
-    delay(DEBOUNCE_TIME);//debounce rising edge
+     // Recognize as knock if maximum of absolute amplitude in samplig window exceeds the short time average + THRESHOLD
+     if (millis()-lastKnock>SAMPLE_WINDOW + 7 && maxAbsVol-(averageShort + THRESHOLD)>0){// if knock detected
+        Serial.println("Knock detected");
+        if(!startTimeSet){
+          startTimeSet=true;
+          startTime = millis();
+          lastKnock = startTime;
+        }
+        else{
+          rawMelody[i] = millis() - lastKnock;
+          //Serial.println(String(i) + ": " + String(rawMelody[i]));
+          i++;
+          if (i == MELODY_LENGTH){ //if whole melody successfully recorded
+            return true;
+          }
+        }
+        lastKnock=millis();
+     }
+     /* //For graphing in Arduino (ctrl+shift+L)
+     Serial.println(maxAbsVol);
+     Serial.print('\t');
+     Serial.print(averageShort);
+     Serial.print('\t');
+     Serial.println(averageLong);
+     */
   }
+  return false;//timed out
 }
 
 void normalizeMelody(unsigned long rawMelody[], float normalizedMelody[]) {
